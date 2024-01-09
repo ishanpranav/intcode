72ae5d751f2e6c260c28835042df2f8c4febabdf
Ishan Pranav <ishan.pranav@outlook.com>
2024-01-09 13:39:10 -0800

    Add day9b

---
+++ src/day09b.sh
@@ -0,0 +1,9 @@
+# Author: Ishan Pranav
+# Copyright (c) 2023 Ishan Pranav
+# Licensed under the MIT license.
+
+# Sensor Boost Part 2
+# Usage: ./day09a.sh <input_path>
+
+echo -n "09b "
+echo 2 | ./../intcode.o $1
--- tools/test.sh
+++ tools/test.sh
@@ -5,3 +5,4 @@ cat ../alt02.csv | .././day02b.o
 cat ../alt07.csv | .././day07a.o
 cat ../alt07.csv | .././day07b.o
 ../src/./day09a.sh ../alt09.csv
+../src/./day09b.sh ../alt09.csv
